bc62f29f7a08e8a555ed16d7eb8d1625f9e304d0
sazidansari9634 <sazidansari9634@gmail.com>
2022-02-10 11:56:56 +0530

    Showing Alert Dialogs conform order delete No or Yes option

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -29,6 +29,31 @@ class CartItem extends StatelessWidget {
             vertical: 4,
           )),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text(
+              'Do You want to remove the item from the cart?',
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+              ),
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
