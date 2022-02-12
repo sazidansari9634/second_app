52512a9f3f08033f8a92b12b451d1b05d5c87cc0
sazidansari9634 <sazidansari9634@gmail.com>
2022-02-12 11:51:52 +0530

    print the data pass in debug console and check

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -65,7 +65,8 @@ class Products with ChangeNotifier {
   void addProduct(Product product) {
     final url = Uri.parse(
         'https://flutter-update-714fa-default-rtdb.firebaseio.com/products.json');
-    http.post(
+    http
+        .post(
       url,
       body: json.encode({
         'title': product.title,
@@ -74,19 +75,21 @@ class Products with ChangeNotifier {
         'price': product.price,
         'isFavorite': product.isFavorite,
       }),
-    );
+    )
+        .then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: DateTime.now().toString(),
+      );
 
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
+    });
   }
 
   Product findById(String id) {
