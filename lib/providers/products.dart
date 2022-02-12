a9067bb9c871d93b5fc6062cb54355362df7d669
sazidansari9634 <sazidansari9634@gmail.com>
2022-02-12 17:03:54 +0530

    [200~246. Handling Errors Gracefully~

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -87,6 +87,9 @@ class Products with ChangeNotifier {
       _items.add(newProduct);
       // _items.insert(0, newProduct); // at the start of the list
       notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
     });
   }
 
