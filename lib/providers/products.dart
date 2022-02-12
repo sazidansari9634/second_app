93cae876040d8bc7a5f33aad0739fb9b677be112
sazidansari9634 <sazidansari9634@gmail.com>
2022-02-12 18:24:10 +0530

    248. Fetching Data, initState & "of(context)"

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -62,9 +62,20 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  Future<void> fetchAndSetProducts() async {
+    final url = Uri.parse(
+        'https://flutter-update-714fa-default-rtdb.firebaseio.com/products.json');
+    try {
+      final response = await http.get(url);
+      print(json.decode(response.body));
+    } catch (error) {
+      throw (error);
+    }
+  }
+
   Future<void> addProduct(Product product) async {
     final url = Uri.parse(
-        'https://flutter-update-714fa-default-rtdb.firebaseio.com/products');
+        'https://flutter-update-714fa-default-rtdb.firebaseio.com/products.json');
     try {
       final response = await http.post(
         url,
